fe5f1b86d41f79008c615f4d57bdd0e0ea38c2e7
brettmadrid <brettmadrid9@gmail.com>
2020-09-04 23:25:11 -0700

    completed registration form with vreact-hook-form validation

--- login-register/src/components/login/login.js
+++ login-register/src/components/login/login.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import loginImg from '../../assets/login.svg'
+// import { Input } from './input'
 
 export const Login = ({ containerRef }) => {
+  const defaultState = {
+    username: '',
+    email: '',
+    password: '',
+  }
+  const [formState, setFormState] = useState(defaultState)
+
+  // onSubmit function
+  const formSubmit = e => {
+    e.preventDefault()
+    console.log('form submitted')
+    setFormState({ username: '', email: '', password: '' })
+  }
+
+  const inputChange = e => {
+    console.log('input changed', e.target.value)
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+  }
   return (
     <div className='base-container' ref={containerRef}>
       <div className='header'>Login</div>
--- login-register/src/components/login/register.js
+++ login-register/src/components/login/register.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import loginImg from '../../assets/login.svg'
 
 export const Register = ({ containerRef }) => {
+  const { register, handleSubmit, errors } = useForm()
+
+  // onSubmit function
+  const onSubmit = data => {
+    console.log('data', data)
+  }
+
+  const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  const validateUserName = async value => {
+    await sleep(1000)
+    if (value === 'Brett') return true
+    return false
+  }
+
   return (
     <div className='base-container' ref={containerRef}>
       <div className='header'>Register</div>
@@ -9,25 +27,77 @@ export const Register = ({ containerRef }) => {
         <div className='image'>
           <img src={loginImg} alt='Login pic' />
         </div>
-        <div className='form'>
-          <div className='form-group'>
-            <label htmlFor='username'>Username</label>
-            <input type='text' name='username' placeholder='username' />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='form'>
+            <div className='form-group'>
+              <label htmlFor='username'>Username</label>
+              <input
+                name='username'
+                placeholder='username'
+                ref={register({
+                  required: true,
+                  minLength: 2,
+                  pattern: /^[a-z]([0-9][0-9]+|[a-z]+\d*)$/i,
+                  validate: validateUserName,
+                })}
+              />
+              {errors.username && errors.username.type === 'required' && (
+                <p>Username is required</p>
+              )}
+              {errors.username && errors.username.type === 'minLength' && (
+                <p>Must be at least 2 characters long</p>
+              )}
+              {errors.username && errors.username.type === 'pattern' && (
+                <p>alphanumeric characters only</p>
+              )}
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input
+                name='email'
+                placeholder='email'
+                ref={register({
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/,
+                })}
+              />
+              {errors.email && errors.email.type === 'required' && (
+                <p>Please enter a valid email</p>
+              )}
+              {errors.email && errors.email.type === 'pattern' && (
+                <p>Please enter a valid email</p>
+              )}
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                name='password'
+                placeholder='password'
+                ref={register({
+                  required: true,
+                  minLength: 8,
+                  pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/,
+                })}
+              />
+              {errors.password && errors.password.type === 'required' && (
+                <p>Password required</p>
+              )}
+              {errors.password && errors.password.type === 'minLength' && (
+                <p>Must be at least 8 characters long</p>
+              )}
+              {errors.password && errors.password.type === 'pattern' && (
+                <p>Must contain a letter, a number, and a special character</p>
+              )}
+            </div>
           </div>
-          <div className='form-group'>
-            <label htmlFor='username'>Email</label>
-            <input type='email' name='email' placeholder='email' />
+          <div className='footer'>
+            <button type='submit' className='btn'>
+              Register
+            </button>
           </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' placeholder='password' />
-          </div>
-        </div>
-        <div className='footer'>
-          <button type='submit' className='btn'>
-            Register
-          </button>
-        </div>
+        </form>
       </div>
     </div>
   )
